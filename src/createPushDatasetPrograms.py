b90c5e72d5737d23b739b53e52a30438df4291e1
Rohan Vashisht <rohanprogrammer1@gmail.com>
2025-04-03 00:50:17 +0530

    Update dataset file path in pushDataset function

--- src/createPushDatasetPrograms.py
+++ src/createPushDatasetPrograms.py
@@ -7,7 +7,7 @@ def pushDataset():
     """
     Push the dataset to a file
     """
-    dataset = json.load(open(constants.PROGRAMS_JSON_FILES[0], "r"))
+    dataset = json.load(open("./jsons/" + constants.PROGRAMS_JSON_FILES[0], "r"))
 
     hfDataset = Dataset.from_list(dataset)
     hfDataset.push_to_hub("zigistry/programs", token=constants.HUGGING_FACE_API_KEY)
