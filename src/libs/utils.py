b128f8e6975c7f23d2d602af85597425dd927da6
Rohan Vashisht <rohanprogrammer1@gmail.com>
2025-05-13 17:15:49 +0530

    added ReadmePurification for better cyber security from python itself.

--- src/libs/utils.py
+++ src/libs/utils.py
@@ -6,7 +6,10 @@ from libs.z2j import get_repo_zon_metadata
 from typing import List, Optional, Dict, Any
 import re
 from libs.types import Dependency
-
+import re
+import markdown
+import bleach
+from bleach.sanitizer import Cleaner
 
 def convertGithubRepoFormToZigistryRepoForm(g: Dict[str, Any]) -> Repo:
     """
@@ -246,3 +249,57 @@ def extract_repo_info(location: str) -> Optional[Dict[str, str]]:
 
     # No match found
     return None
+
+
+def convert2markdown(x: str) -> str:
+    x = re.sub(r"- \[x\]", "±§±§±§±", x)
+    x = re.sub(r"- \[ \]", "±§±§±§§±", x)
+
+    content = markdown.markdown(x)
+
+    cleaner = Cleaner(
+        tags=bleach.sanitizer.ALLOWED_TAGS,
+        attributes={
+            "a": ["name", "target"],
+            "code": ["class"],
+            "img": ["src", "srcset", "alt", "title", "width", "height", "loading"],
+        },
+        strip=True
+    )
+    content = cleaner.clean(content)
+
+    content = content.replace(
+        "±§±§±§§±",
+        "<br/><input type='checkbox' class='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600' disabled></input>",
+    )
+
+    content = content.replace(
+        "±§±§±§±",
+        "<br/><input type='checkbox' class='w-4 h-4 text-green-500 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600' checked disabled></input>",
+    )
+
+    content = re.sub(
+        r"\[!IMPORTANT\]",
+        '<span class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">IMPORTANT</span>',
+        content,
+    )
+
+    content = re.sub(
+        r"\[!NOTE\]",
+        '<span class="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">NOTE</span>',
+        content,
+    )
+
+    content = re.sub(
+        r"\[!WARNING\]",
+        '<span class="bg-yellow-100 text-yellow-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300">WARNING</span>',
+        content,
+    )
+
+    content = re.sub(
+        r"\[!CAUTION\]",
+        '<span class="bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300">CAUTION</span>',
+        content,
+    )
+
+    return content
