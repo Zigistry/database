e4457f759446cbc5ce7bd577098a1beec55051dd
Rohan Vashisht <rohanprogrammer1@gmail.com>
2025-12-13 01:48:44 +0530

    Codeberg release code

--- src/codeberg_process_release.rs
+++ src/codeberg_process_release.rs
@@ -10,11 +10,15 @@ use crate::constants::POSSIBLE_README_FILE_NAMES;
 use crate::custom_types;
 use crate::custom_types::Dependency;
 use lazy_static::lazy_static;
+
 lazy_static! {
-    static ref KEY2:String = String::new();
+    static ref KEY2: String = String::new();
 }
 
-pub async fn process_release(owner_name: String, repo_name: String) -> Result<custom_types::Release, Box<dyn std::error::Error>> {
+pub async fn process_release(
+    owner_name: String,
+    repo_name: String,
+) -> Result<custom_types::Release, Box<dyn std::error::Error>> {
     let release_url = format!(
         "https://codeberg.org/api/v1/repos/{}/{}/releases",
         owner_name, repo_name
@@ -29,25 +33,40 @@ pub async fn process_release(owner_name: String, repo_name: String) -> Result<cu
     // https://codeberg.org/FObersteiner/zdt/raw/tag/v0.8.2-zig_0.15/README.md
     // https://codeberg.org/FObersteiner/zdt/raw/tag/v0.8.2-zig_0.15/build.zig.zon
     for i in client {
-        let bzz_link = format!("https://codeberg.org/{owner_name}/{repo_name}/raw/tag/{}/build.zig.zon", i.tag_name);
+        let bzz_link = format!(
+            "https://codeberg.org/{owner_name}/{repo_name}/raw/tag/{}/build.zig.zon",
+            i.tag_name
+        );
         let bzz_stuff = reqwest::Client::new()
-        .get(&bzz_link)
-        .send()
-        .await?
-        .text()
-        .await?;
+            .get(&bzz_link)
+            .send()
+            .await?
+            .text()
+            .await?;
 
-        let compiled_release = custom_types::Release {
-            dependencies: vec![],
-            is_prerelease:i.prerelease,
-            published_at:i.published_at,
-            release_assets:HashMap::new(),
-            minimum_zig_version:,
-            readme_url: get_readme_url(&owner_name, &repo_name, &i.tag_name, true).await,
+        let details = match get_build_zig_zon_data(&owner_name, &repo_name, &i.tag_name, true).await
+        {
+            Ok(r) => r,
+            Err(_) => (String::new(), Vec::new()),
         };
 
+        let compiled_release = custom_types::Release {
+            dependencies: details.1,
+            is_prerelease: i.prerelease,
+            published_at: i.published_at,
+            release_assets: HashMap::new(),
+            // the issue is that, Idk the type of aset
+            // wasn't able to find a single repo with
+            // some actual asset, hence, I am leaving this
+            // as "Value" only, putting a normal hash new
+            // I will figure this out after first release.
+            // the code is already spoilt, need to very
+            // complicated at this point.
+            minimum_zig_version: details.0,
+            readme_url: get_readme_url(&owner_name, &repo_name, &i.tag_name, true).await,
+        };
     }
-    
+
     Ok()
 }
 
@@ -134,21 +153,16 @@ pub struct ArchiveDownloadCount {
     pub tar_gz: i64,
 }
 
-
 pub async fn get_readme_url(
     owner_name: &str,
     repo_name: &str,
     branch_or_tag: &str,
-    is_tag:bool,
+    is_tag: bool,
 ) -> String {
     let url = if is_tag {
-        format!(
-            "https://codeberg.org/{owner_name}/{repo_name}/raw/tag/{branch_or_tag}/"
-        )
+        format!("https://codeberg.org/{owner_name}/{repo_name}/raw/tag/{branch_or_tag}/")
     } else {
-        format!(
-            "https://codeberg.org/{owner_name}/{repo_name}/raw/branch/{branch_or_tag}/"
-        )
+        format!("https://codeberg.org/{owner_name}/{repo_name}/raw/branch/{branch_or_tag}/")
     };
 
     let client = reqwest::Client::new();
@@ -164,14 +178,11 @@ pub async fn get_readme_url(
     String::new()
 }
 
-
-
-
 pub async fn get_build_zig_zon_data(
     owner_name: &str,
     repo_name: &str,
     branch_or_tag: &str,
-    is_tag:bool,
+    is_tag: bool,
 ) -> Result<(String, Vec<Dependency>), Box<dyn Error>> {
     let url = if is_tag {
         format!(
@@ -183,13 +194,13 @@ pub async fn get_build_zig_zon_data(
             "https://codeberg.org/{owner_name}/{repo_name}/raw/branch/{branch_or_tag}/build.zig.zon"
         )
     };
-   
+
     let client = reqwest::Client::new();
     let text = client.get(&url).send().await?.text().await?;
 
-    let tokens = crate::bzz_stuff::tokenize(&mut text.chars().collect::<Vec<_>>().into_iter().peekable())?;
+    let tokens =
+        crate::bzz_stuff::tokenize(&mut text.chars().collect::<Vec<_>>().into_iter().peekable())?;
     let parsed = parse(&mut tokens.into_iter().peekable())?;
 
     Ok((parsed.minimum_zig_version, parsed.dependencies))
 }
-
