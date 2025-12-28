637065a60308306b88523812dc4319f9d534cdbe
Rohan Vashisht <rohanprogrammer1@gmail.com>
2025-12-28 20:16:48 +0530

    Trying to add dependents calculator version 2

--- src/dependents_calculator.rs
+++ src/dependents_calculator.rs
@@ -15,6 +15,22 @@ pub fn calculate_dependents() -> void {
         for m in vec_of_all_urls_of_this_package {
             // I will now find the gh/thingy/thingy pattern
             // Ex: github.com/zigzap/zap
+            let gh_cb_pattern = Regex::new(
+                r"(?i)\b(?:https?://)?(?:www\.)?(github\.com|codeberg\.org)/([A-Za-z0-9_.-]+)/([A-Za-z0-9_.-]+)/?\b"
+            ).unwrap();
+            let captures = gh_cb_pattern.captures(m);
+            if let Some(c) = captures {
+                let provider = c.get(1).map(|p| p.as_str());
+                let owner = c.get(2).map(|p| p.as_str());
+                let repo = c.get(3).map(|p| p.as_str());
+                if let (Some(provider), Some(owner), Some(repo)) = (provider, owner, repo) {
+                    if provider == "github.com" {
+                        let key = format!("gh/{provider}/{repo}");
+                    }
+                } else {
+                    continue;
+                }
+            }
         }
     }
 }
