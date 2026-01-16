7f25ac25469b4482cde378dc0073e891bdc703b3
Rohan Vashisht <rohanprogrammer1@gmail.com>
2026-01-16 10:25:25 +0530

    Made the database schema

--- databaseSchema.sql
+++ databaseSchema.sql
@@ -1,15 +1,73 @@
-CREATE TABLE repo (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    provider TEXT NOT NULL,
-    issue_count INT NOT NULL,
-    fork_count INT NOT NULL,
-    stargazers_count INT NOT NULL,
-    watchers_count INT NOT NULL,
-    pushed_at TIMESTAMP NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    is_archived BOOLEAN NOT NULL,
-    is_disabled BOOLEAN NOT NULL,
-    is_fork BOOLEAN NOT NULL,
-    license TEXT NOT NULL,
+
+CREATE TABLE repos (
+  id TEXT PRIMARY KEY,
+  owner TEXT NOT NULL,
+  avatar_id TEXT NOT NULL,
+  description TEXT,
+  issues_count INTEGER NOT NULL,
+  default_branch_name TEXT NOT NULL,
+  fork_count INTEGER NOT NULL,
+  stargazer_count INTEGER NOT NULL,
+  watchers_count INTEGER NOT NULL,
+  pushed_at TEXT NOT NULL,
+  created_at TEXT NOT NULL,
+  is_archived INTEGER NOT NULL,
+  is_disabled INTEGER NOT NULL,
+  is_fork INTEGER NOT NULL,
+  license TEXT NOT NULL,
+  minimum_zig_version TEXT,
+  readme_url TEXT,
+  primary_language TEXT NOT NULL,
+  FOREIGN KEY (owner) REFERENCES users(id)
 )
-CREATE TABLE 
+
+CREATE TABLE repo_topics (
+  repo_id TEXT NOT NULL,
+  topic TEXT NOT NULL,
+  FOREIGN KEY (repo_id) REFERENCES repos(id)
+)
+
+CREATE TABLE users (
+  id TEXT PRIMARY KEY,
+  avatar_id TEXT NOT NULL,
+  bio TEXT,
+  company TEXT,
+  followers INTEGER NOT NULL,
+  following INTEGER NOT NULL,
+  location TEXT,
+  description TEXT,
+  website_url TEXT
+)
+
+CREATE TABLE repo_dependents (
+  repo_id TEXT NOT NULL,
+  dependent TEXT NOT NULL,
+  FOREIGN KEY (repo_id) REFERENCES repos(id)
+)
+
+
+CREATE TABLE releases (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  repo_id TEXT NOT NULL,
+  version TEXT NOT NULL,
+  is_prerelease INTEGER NOT NULL,
+  published_at TEXT NOT NULL,
+  minimum_zig_version TEXT NOT NULL,
+  readme_url TEXT NOT NULL,
+  FOREIGN KEY (repo_id) REFERENCES repos(id)
+)
+CREATE TABLE release_dependencies (
+  release_id INTEGER NOT NULL,
+  name TEXT NOT NULL,
+  hash TEXT NOT NULL,
+  lazy TEXT NOT NULL,
+  url TEXT NOT NULL,
+  path TEXT NOT NULL,
+  FOREIGN KEY (release_id) REFERENCES releases(id)
+)
+
+CREATE TABLE index_sections (
+  section_name TEXT NOT NULL,
+  repo_id TEXT NOT NULL,
+  FOREIGN KEY (repo_id) REFERENCES repos(id)
+)
