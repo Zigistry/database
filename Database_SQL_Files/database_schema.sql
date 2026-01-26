afebca37da1190814c90545ce99c38f3e07069ee
Rohan Vashisht <rohanprogrammer1@gmail.com>
2026-01-26 22:37:12 +0530

    I think this is the schema which will work.

--- Database_SQL_Files/database_schema.sql
+++ Database_SQL_Files/database_schema.sql
@@ -31,16 +31,18 @@ CREATE TABLE repos (
   watchers_count INTEGER NOT NULL,
   pushed_at TIMESTAMP NOT NULL,
   created_at TIMESTAMP NOT NULL,
-  is_archived BOOLEAN NOT NULL,
-  is_disabled BOOLEAN NOT NULL,
-  is_fork BOOLEAN NOT NULL,
+  -- I just realised that the boolean type is like an integer only.
+  -- I added check that it should only be either true or false. i.e 0, 1
+  is_archived BOOLEAN NOT NULL CHECK (is_archived IN (0,1)),
+  is_disabled BOOLEAN NOT NULL CHECK (is_disabled IN (0,1)),
+  is_fork BOOLEAN NOT NULL CHECK (is_fork IN (0,1)),
   minimum_zig_version TEXT NOT NULL, -- Again, this I have added a join to make sure
   -- I don't have to read the default branch information just to get this info.
   -- Simple google tells, its just 36 characters, so, ok?
   license VARCHAR(40) NOT NULL,
   -- Maybe 50 is perfect here
   primary_language VARCHAR(50) NOT NULL,
-  FOREIGN KEY (owner) REFERENCES users (id)
+  FOREIGN KEY (owner) REFERENCES users (id) ON DELETE CASCADE
 );
 
 CREATE VIRTUAL TABLE repo_search USING fts5 (
@@ -58,18 +60,25 @@ WHERE
 
 END;
 
+CREATE TRIGGER repo_delete_cleanup
+AFTER DELETE ON repos
+BEGIN
+  DELETE FROM repo_search
+  WHERE repo_id = OLD.id;
+END;
+
 CREATE TABLE repo_topics (
   repo_id VARCHAR(150) NOT NULL,
   -- Limit is 50, hence
   topic VARCHAR(60) NOT NULL,
-  FOREIGN KEY (repo_id) REFERENCES repos (id),
+  FOREIGN KEY (repo_id) REFERENCES repos (id) ON DELETE CASCADE,
   UNIQUE (repo_id, topic)
 );
 
 CREATE TABLE repo_dependents (
   repo_id VARCHAR(150) NOT NULL,
   dependent TEXT NOT NULL,
-  FOREIGN KEY (repo_id) REFERENCES repos (id),
+  FOREIGN KEY (repo_id) REFERENCES repos (id) ON DELETE CASCADE,
   UNIQUE (repo_id, dependent)
 );
 
@@ -81,7 +90,7 @@ CREATE TABLE releases (
   published_at TIMESTAMP NOT NULL,
   minimum_zig_version TEXT NOT NULL,
   readme_url TEXT NOT NULL,
-  FOREIGN KEY (repo_id) REFERENCES repos (id),
+  FOREIGN KEY (repo_id) REFERENCES repos (id) ON DELETE CASCADE,
   UNIQUE (repo_id, version)
 );
 
@@ -92,25 +101,25 @@ CREATE TABLE release_dependencies (
   lazy TEXT NOT NULL,
   url TEXT NOT NULL,
   path TEXT NOT NULL,
-  FOREIGN KEY (release_id) REFERENCES releases (id),
+  FOREIGN KEY (release_id) REFERENCES releases (id) ON DELETE CASCADE,
   UNIQUE (release_id, name)
 );
 
 CREATE TABLE index_sections (
   section_name VARCHAR(10) NOT NULL,
   repo_id VARCHAR(150) NOT NULL,
-  FOREIGN KEY (repo_id) REFERENCES repos (id),
+  FOREIGN KEY (repo_id) REFERENCES repos (id) ON DELETE CASCADE,
   UNIQUE (section_name, repo_id)
 );
 
 CREATE TABLE packages (
   repo_id VARCHAR(150) NOT NULL PRIMARY KEY,
-  FOREIGN KEY (repo_id) REFERENCES repos (id)
+  FOREIGN KEY (repo_id) REFERENCES repos (id) ON DELETE CASCADE
 );
 
 CREATE TABLE programs (
   repo_id VARCHAR(150) NOT NULL PRIMARY KEY,
-  FOREIGN KEY (repo_id) REFERENCES repos (id)
+  FOREIGN KEY (repo_id) REFERENCES repos (id) ON DELETE CASCADE
 );
 
 -- I do search for "All repos where username is username"
@@ -135,3 +144,8 @@ CREATE INDEX repo_topics_repo_id_index ON repo_topics (repo_id);
 
 -- Ok, on every package I also need number of repo dependents.
 CREATE INDEX repo_dependents_repo_id_index ON repo_dependents (repo_id);
+
+-- I do search for "All packages where username is username", and also
+-- for  "All programs where username is username"
+CREATE INDEX packages_repo_id_index ON packages (repo_id);
+CREATE INDEX programs_repo_id_index ON programs (repo_id);
