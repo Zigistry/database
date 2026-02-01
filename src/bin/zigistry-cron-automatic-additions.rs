7422132f9033ce505a66b8a21b2e81e2b374fffe
Rohan Vashisht <rohanprogrammer1@gmail.com>
2026-02-01 20:07:50 +0530

    added a new timestamp sender

--- src/bin/zigistry-cron-automatic-additions.rs
+++ src/bin/zigistry-cron-automatic-additions.rs
@@ -2,21 +2,29 @@ use actix_web::App;
 use actix_web::HttpServer;
 use actix_web::Responder;
 use actix_web::get;
+use actix_web::web;
+use chrono::NaiveDateTime;
 use chrono::Utc;
 use std::error::Error;
 use std::sync::Arc;
+use std::sync::RwLock;
 use zigistry::codeberg;
 use zigistry::database;
 use zigistry::github;
 
 #[get("/")]
-async fn index() -> impl Responder {
-    "Status: Active"
+async fn index(last_updated: actix_web::web::Data<Arc<RwLock<NaiveDateTime>>>) -> impl Responder {
+    format!("Status: Active {}", last_updated.read().unwrap())
 }
 
 #[actix_web::main]
 pub async fn main() -> Result<(), Box<dyn Error>> {
     let pool = Arc::new(database::connect_to_database().await.unwrap());
+
+    let last_time_stamp = Arc::new(RwLock::new(
+        Utc::now().naive_utc() - chrono::Duration::minutes(30),
+    ));
+    let last_time_stamp_clone = Arc::clone(&last_time_stamp);
 
     let orig_hook = std::panic::take_hook();
     std::panic::set_hook(Box::new(move |panic_info| {
@@ -27,36 +35,36 @@ pub async fn main() -> Result<(), Box<dyn Error>> {
     actix_web::rt::spawn(async move {
         // I am doing  - chrono::Duration::minutes(15) to make sure
         // If the api takes some time to update, that is still covered.
-        let mut last_time_stamp = Utc::now().naive_utc() - chrono::Duration::minutes(30);
         loop {
             let current_time = Utc::now().naive_utc() - chrono::Duration::minutes(15);
-            codeberg::process_last_15_minutes("zig", last_time_stamp, Arc::clone(&pool)).await;
-            codeberg::process_last_15_minutes("zig-package", last_time_stamp, Arc::clone(&pool))
+            let last_ts = *last_time_stamp_clone.read().unwrap();
+
+            codeberg::process_last_15_minutes("zig", last_ts, Arc::clone(&pool)).await;
+            codeberg::process_last_15_minutes("zig-package", last_ts, Arc::clone(&pool)).await;
+            github::process_last_15_minutes(Arc::clone(&pool), "zig".to_string(), false, last_ts)
                 .await;
-            github::process_last_15_minutes(
-                Arc::clone(&pool),
-                "zig".to_string(),
-                false,
-                last_time_stamp,
-            )
-            .await;
             github::process_last_15_minutes(
                 Arc::clone(&pool),
                 "zig-package".to_string(),
                 true,
-                last_time_stamp,
+                last_ts,
             )
             .await;
-            // Now I also need to update https://codeberg.org/api/v1/repos/search?q=zig-package&sort=updated&order=desc&limit=50&page=1
-            last_time_stamp = current_time;
+
+            *last_time_stamp_clone.write().unwrap() = current_time;
 
             actix_web::rt::time::sleep(std::time::Duration::from_secs(900)).await;
         }
     });
 
-    let my_server = HttpServer::new(|| App::new().service(index))
-        .bind(("0.0.0.0", 7860))
-        .unwrap();
+    let my_server = HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(Arc::clone(&last_time_stamp)))
+            .service(index)
+    })
+    .bind(("0.0.0.0", 7860))
+    .unwrap();
+
     println!("Server at: http://0.0.0.0:7860");
     my_server.run().await.unwrap();
     Ok(())
