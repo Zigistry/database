961af0e6bdfce15612a1d347b252992a54aaa98e
RohanVashisht1234 <rohanprogrammer1@gmail.com>
2026-06-11 17:48:59 +0000

    changed remote db to local db

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -27,11 +27,7 @@ pub fn utc_now_timestamp() -> String {
 
 pub async fn connect_to_database() -> Result<Connection, Box<dyn std::error::Error>> {
     dotenv().ok();
-    let db = Builder::new_remote_replica(
-        "./zigistry.db",
-        env::var("DATABASE_URL").expect("DATABASE_URL not found"),
-        env::var("API_KEY").expect("API_KEY not found"),
-    );
+    let db = Builder::new_local("./zigistry.db");
     let client = db.build().await.unwrap();
     let pool = client.connect().unwrap();
     if START_FROM_SCRATCH {
